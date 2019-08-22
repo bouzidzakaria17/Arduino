ae284f37173abbdb0c97682cddbc691758b32ef8
Zachariah <bouzidzakaria17@yahoo.fr>
2019-08-22 19:01:25 +0100

    Update Stepper_Motor_Code.ino

--- Stepper_Motor_Code.ino
+++ Stepper_Motor_Code.ino
@@ -1,19 +1,13 @@
-// defines pins numbers
-
+// Define Step annd Direction Pins
 const int stepPin = 3;
 const int dirPin = 6;
-int ledPin= 13;
 
 void setup() {
   // Sets the two pins as Outputs
   pinMode(stepPin, OUTPUT);
-  pinMode(dirPin, OUTPUT);
-  pinMode(ledPin, OUTPUT);
-  Serial.begin(9600);
+  pinMode(dirPin, OUTPUT)
 }
 void loop() {
-
-  
   digitalWrite(dirPin, HIGH); // Enables the motor to move in a particular direction
   // Makes 200 pulses for making one full cycle rotation
   for (int x = 0; x < 200; x++) {
@@ -22,10 +16,8 @@ void loop() {
     delayMicroseconds(500);
     digitalWrite(stepPin, LOW);
     delayMicroseconds(500);
-
   }
   delay(2000); // One second delay
-
     digitalWrite(dirPin, LOW); //Changes the rotations direction
   // Makes 400 pulses for making two full cycle rotation
   for (int x = 0; x < 400; x++) {
@@ -36,5 +28,4 @@ void loop() {
   digitalWrite(ledPin, HIGH);
   }
   delay(2000);
-
 }
